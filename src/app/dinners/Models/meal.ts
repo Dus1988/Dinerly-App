955567ae44cb14c093a547a9d6db37ff6af36391
Dustin Godin <dustin.godin@gmail.com>
2019-09-02 21:24:08 -0400

    add meal type

--- src/app/dinners/Models/meal.ts
+++ src/app/dinners/Models/meal.ts
@@ -1,16 +1,20 @@
 
 import { CopyUtil } from 'src/app/shared/utils/copy-util';
+import { MealTypes } from '../components/enums/meal-types.enum';
 
 export class IMeal {
   description: string;
-
+  mealType?: string;
 }
 
 export class Meal implements IMeal {
+
   public description: string;
+  public mealType: string;
 
   constructor(options?: IMeal) {
     this.description = '';
+    this.mealType = MealTypes.Dinner;
     if (options) {
       CopyUtil.copyProps(options, this);
     }
---
+++ src/app/dinners/components/enums/meal-types.enum.ts
@@ -0,0 +1,6 @@
+export enum MealTypes {
+  Breakfast = 'B',
+  Lunch = 'L',
+  Dinner = 'D',
+  Snack = 'S'
+}
