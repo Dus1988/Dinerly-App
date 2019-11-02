60b66671d8bd9d33b91e84808669f629c9a8a52d
Dustin Godin <dustin.godin@gmail.com>
2019-11-02 15:44:16 -0400

    overlay animation slide

--- src/app/shared/Components/generic-overlay/generic-overlay.component.ts
+++ src/app/shared/Components/generic-overlay/generic-overlay.component.ts
@@ -8,23 +8,31 @@ import { Component, OnInit, ContentChild, TemplateRef, Output, EventEmitter, Inp
 export class GenericOverlayComponent implements OnInit {
 
   @ContentChild('body') public bodyTemplate: TemplateRef<any>;
-  public open: boolean;
   @Input()
   header: string;
   @Output()
   OverlayClosed: EventEmitter<any>;
 
+  public open: boolean;
+  public closing: boolean;
+
   constructor() {
     this.OverlayClosed = new EventEmitter();
     this.open = true;
+    this.closing = false;
    }
 
   ngOnInit() {
   }
 
   closeOverlay() {
-    this.open = false;
-    this.OverlayClosed.emit();
+    this.closing = true;
+    setTimeout(() => {
+
+      this.open = false;
+      this.OverlayClosed.emit();
+    }, 300);
+
   }
 
 }
